55717aae1851b172f54c2f2d6690af3f4c4c6768
Laurel YAMA <161228377+laurelYama@users.noreply.github.com>
2025-05-14 16:52:52 +0100

    Update CorsConfig.java

--- src/main/java/com/esiitech/trombinoscope_api/config/CorsConfig.java
+++ src/main/java/com/esiitech/trombinoscope_api/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:4200") // frontend URL
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
